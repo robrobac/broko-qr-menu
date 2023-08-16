341d58520b4bd32fd45024ce108e7f3864c0304f
Roberto <roberto.vukomanovic@gmail.com>
2023-08-16 22:58:53 +0200

    added language toggle, saves to local storage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logout from './Logout'
 import Logo from "../images/brokologo.png"
-import { HeaderLogo, HeaderWrap } from './styledComponents/styledHeader'
+import { HeaderLogo, HeaderRight, HeaderWrap, LanguageIcon, LanguageSelect, LanguageTitle, LanguageTitleWrap } from './styledComponents/styledHeader'
+import { ReactComponent as GlobeIcon } from "../icons/globeicon.svg";
+import { useTranslation } from 'react-i18next';
 
 function Header() {
+    const { i18n } = useTranslation()
+    const [selectedLanguage, setSelectedLanguage] = useState("hr")
+
+    useEffect(() => {
+        const cachedData = localStorage.getItem("selectedLanguage");
+
+        if (cachedData) {
+            const parsedData = JSON.parse(cachedData)
+            setSelectedLanguage(parsedData.selectedLanguage)
+            i18n.changeLanguage(parsedData.selectedLanguage)
+        }     
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const handleLanguage = () => {
+        if (selectedLanguage === "hr") {
+            setSelectedLanguage("en")
+            i18n.changeLanguage("en")
+
+            const dataToCache = {
+                selectedLanguage: "en",
+            };
+
+            localStorage.setItem('selectedLanguage', JSON.stringify(dataToCache));
+        } else if (selectedLanguage === "en") {
+            setSelectedLanguage("hr")
+            i18n.changeLanguage("hr")
+
+            const dataToCache = {
+                selectedLanguage: "hr",
+            };
+
+            localStorage.setItem('selectedLanguage', JSON.stringify(dataToCache));
+        }
+    }
+
     return (
         <HeaderWrap>
             <HeaderLogo src={Logo} alt='logo' className='logoImg'></HeaderLogo>
-            <Logout />
+            <HeaderRight>
+                <LanguageSelect onClick={() => handleLanguage()}>
+                    <LanguageIcon>
+                        <GlobeIcon/>
+                    </LanguageIcon>
+                    <LanguageTitleWrap >
+                        <LanguageTitle $isActive={selectedLanguage === "hr" ? "true" : undefined}>hr</LanguageTitle>
+                        <LanguageTitle>|</LanguageTitle>
+                        <LanguageTitle $isActive={selectedLanguage === "en" ? "true" : undefined}>en</LanguageTitle>
+                    </LanguageTitleWrap>
+                </LanguageSelect>
+                <Logout />
+            </HeaderRight>
         </HeaderWrap>
     )
 }
--- src/components/styledComponents/styledHeader.js
+++ src/components/styledComponents/styledHeader.js
@@ -8,6 +8,34 @@ export const HeaderWrap = styled.header`
     align-items: center;
 `
 
+export const HeaderRight = styled.div`
+    display: flex;
+    width: 170px;
+    justify-content: space-between;
+`
+
 export const HeaderLogo = styled.img`
     height: 30px;
+`
+
+export const LanguageSelect = styled.div`
+    display: flex;
+    align-items: center;
+    gap: .5rem;
+`
+export const LanguageTitleWrap = styled.div`
+    display: flex;
+    gap: 5px;
+    color: #5b7e6c;
+`
+export const LanguageTitle = styled.p`
+    color: ${(props) => (props.$isActive) ? "white" : "#5b7e6c"};
+`
+export const LanguageIcon = styled.div`
+    background-color: white;
+    fill: #235239;
+    border-radius: 30px;
+    height: 1.5rem;
+    width: 1.5rem;
+    padding: 1px;
 `
